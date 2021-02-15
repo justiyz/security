f846ca70648ce1571b5672a9e1d68f77a328b941
justiyz <inyange@yahoo.com>
2021-02-15 01:38:59 +0100

    Permission Based Authentication(using @PreAuthorized anotation in the controlllers)

--- src/main/java/com/springsecdemo/web/controllers/studentManagementController/StudentManagementController.java
+++ src/main/java/com/springsecdemo/web/controllers/studentManagementController/StudentManagementController.java
@@ -2,6 +2,7 @@ package com.springsecdemo.web.controllers.studentManagementController;
 
 import com.springsecdemo.data.model.Student;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -19,23 +20,27 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(Student student){
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
 //        log.info(String.valueOf(student));
         log.info("new students --> {}", student);
     }
 
     @DeleteMapping(path = {"{studentId}"})
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         log.info("Deleted student",studentId);
     }
 
     @PutMapping(path = {"{studentId}"})
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
-        log.info(String.format("%s %s", studentId, student)); 
+        log.info(String.format("%s %s", studentId, student));
     }
 }
