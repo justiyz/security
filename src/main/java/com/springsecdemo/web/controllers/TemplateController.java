c94aec53a721312a381d7195af06409b160853fc
justiyz <inyange@yahoo.com>
2021-02-16 13:21:47 +0100

    custom login page for form based authentication

---
+++ src/main/java/com/springsecdemo/web/controllers/TemplateController.java
@@ -0,0 +1,20 @@
+package com.springsecdemo.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+
+    @GetMapping(path = {"login"})
+    public String getLoginView(){
+        return "login";
+    }
+
+    @GetMapping(path = {"courses"})
+    public String getCourses(){
+        return "courses";
+    }
+}
