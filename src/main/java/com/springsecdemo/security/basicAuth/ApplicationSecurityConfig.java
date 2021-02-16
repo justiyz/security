503e785bcf427bfe14ccf60c041625b19afacab9
justiyz <inyange@yahoo.com>
2021-02-16 10:45:42 +0100

    adding formLogin()

--- src/main/java/com/springsecdemo/security/basicAuth/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecdemo/security/basicAuth/ApplicationSecurityConfig.java
@@ -33,15 +33,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/", "index").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Override
